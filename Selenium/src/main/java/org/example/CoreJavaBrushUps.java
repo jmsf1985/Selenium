a64917f0378c91912ff314f408100f13c27df9d5
jmsf1985 <kanemetal98@gmail.com>
2023-05-24 21:02:57 -0400

    Selenium Project

---
+++ Selenium/src/main/java/org/example/CoreJavaBrushUps.java
@@ -0,0 +1,70 @@
+package main.java.org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CoreJavaBrushUps {
+//    public static void main(String[] args) {
+//
+//        int myNumber = 5;
+//        String website = "Rahul Shetty Academy";
+//        char letter = 'r';
+//        double dec = 5.99;
+//        boolean myCard = true;
+//
+//        System.out.println(myNumber + " is the value stored in the myNum variable");
+//        System.out.println(website);
+//
+//        int[] arr = new int[5];
+//        arr[0] = 1;
+//        arr[1] = 2;
+//        arr[2] = 4;
+//        arr[3] = 5;
+//        arr[4] = 6;
+//
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+//
+//        String[] name = {"Jose", "Salinas", "Rahul"};
+//
+//        for(String s: name){
+//            System.out.println(s);
+//        }
+//
+//        int[] arr2 = {1,2,4,5,6,7,8,9,10,122};
+//
+//        for(int i = 0; i < arr2.length; i++){
+//            if(arr2[i] % 2 == 0){
+//                System.out.println(arr2[i]);
+//            }
+//            else{
+//                System.out.println(arr2[i] + " is not even");
+//            }
+//        }
+//
+//        ArrayList<String> a = new ArrayList<String>();
+//        a.add("Jose");
+//        a.add("Salinas");
+//        a.add("Shetty");
+//        a.add("Scarlett");
+//        a.add("Mariah");
+//        System.out.println(a.get(4));
+//
+//        for(String s : a){
+//            System.out.println(s);
+//        }
+//
+//        System.out.println(a.contains("Jose"));
+//        System.out.println(a.contains("Jasmin"));
+//
+//        String[] name = {"Jose", "Juan", "Miguel"};
+//        List nameArrayList = Arrays.asList(name);
+//
+//        for(Object s2 : nameArrayList){
+//            System.out.println(s2);
+//        }
+//
+//    }
+}
---
+++ Selenium/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package main.java.org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver","/Oreilly/chromedriver.exe");
+//
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://rahulshettyacademy.com");
+//        System.out.println(driver.getTitle());
+//        System.out.println(driver.getCurrentUrl());
+//        driver.close();
+//
+////        System.setProperty("webdriver.gecko.driver","/Oreilly/geckodriver.exe");
+////        WebDriver driver = new FirefoxDriver();
+////        driver.get("https://rahulshettyacademy.com");
+////        System.out.println(driver.getTitle());
+////        System.out.println(driver.getCurrentUrl());
+//    }
+//}
